60f8f774d43c2d20664458f160fb24f26588cae8
realBikto <victorhm_94@hotmail.com>
2021-11-08 19:53:38 +0100

    Initial commit

---
+++ src/main/java/thenews/thenews/ThenewsApplication.java
@@ -0,0 +1,13 @@
+package thenews.thenews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThenewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThenewsApplication.class, args);
+	}
+
+}
